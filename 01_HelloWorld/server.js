8acd5e0977ff06aab450b39fe72746ff5af38ad4
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-04-23 22:54:36 +0530

    started express again

--- 01_HelloWorld/server.js
+++ 01_HelloWorld/server.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('This is Home!')
+})
+app.get('/profile', (req, res) => {
+  res.send('This is Profile!')
 })
 
 app.listen(port, () => {
