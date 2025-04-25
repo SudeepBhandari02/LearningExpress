2d7d4285344c175174a94ff29b9f74b89a46cf8c
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-04-25 23:38:19 +0530

    createed a small task to understand dynamic routing with ejs in express

--- 03_formSessionCookies/index.js
+++ 03_formSessionCookies/index.js
@@ -1,24 +1,46 @@
+
 const express = require('express');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
 
-app.use(cookieParser());
+//This code demonstrates how to set and get cookies in an Express.js application using the cookie-parser middleware.
+// const cookieParser = require('cookie-parser');
+// app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('This is Home!');
+// app.get('/', (req, res) => {
+//   res.send('This is Home!');
+// });
+
+// app.get('/set-cookie', (req, res) => {
+//   res.cookie('username', 'john');
+//   res.send('Cookie set');
+// });
+
+// app.get('/get-cookie', (req, res) => {
+//   res.send(`Hello ${req.cookies.username}`);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// })
+
+
+//This code demonstrates how to use sessions in an Express.js application using the express-session middleware.
+
+const session = require('express-session');
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.get('/login', (req, res) => {
+  req.session.username = 'john';
+  res.send('Session started');
 });
 
-app.get('/set-cookie', (req, res) => {
-  res.cookie('username', 'john');
-  res.send('Cookie set');
+app.get('/dashboard', (req, res) => {
+  res.send(`Hello ${req.session.username}`);
 });
-
-app.get('/get-cookie', (req, res) => {
-  res.send(`Hello ${req.cookies.username}`);
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-})
