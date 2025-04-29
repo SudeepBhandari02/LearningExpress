5467ccc8316248fb1051f68527283666a17f1084
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-04-29 20:39:15 +0530

    updated app.js

--- 06_bookManager/app.js
+++ 06_bookManager/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bookRoutes = require('./routes/bookRoutes');
 const errorHandler = require("./middlewares/errorHandler");
+
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
